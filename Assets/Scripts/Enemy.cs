41676361dba80dfdf87c75fc3916bd87c944b15f
m-hayes95 <115077851+m-hayes95@users.noreply.github.com>
2023-03-28 11:32:09 +0100

    Added random spawn and enemy look at centre of screen

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private GameObject centrePoint;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Assign center point in scene view to gameobject var.
+        centrePoint = GameObject.FindGameObjectWithTag("CentrePoint");
+        // Set roatation to look at enemy.
+        transform.LookAt(centrePoint.transform.position);
         // On spawn, assign rigid body and random thrust, using them to add force to the game object.
         float enemyPrefabThrust = Random.Range(100f, 1000f);
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,16 +6,34 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public GameObject spawnLocation;
-    public float spawnRate = 1f;
-    public float nextSpawn = 0f;
-    public float thrust;
    
-    
+    public float spawnRate = 1f, nextSpawn = 0f; // Spawn timer ref
+
+    private float spawnAreaWidth, spawnAreaHeight;
+
+    public Vector3 randomSpawnPoint;
+
+    private void Start()
+    {
+        spawnAreaHeight = 1080;
+        spawnAreaWidth = 1920;
+    }
 
     private void Update()
     {
-        
-        Shoot();
+        /*
+        for (int x=0; x < spawnAreaWidth; x++)
+        {
+            for (int y=0; y < spawnAreaHeight; y++)
+            {
+                // use perlin noise to generate a random spawn location.
+                float perlinValue = Mathf.PerlinNoise(x / spawnAreaWidth, y / spawnAreaHeight);
+                
+                
+            }
+        } */
+
+        Shoot(); // Shoot an instantiated enemy prefab.
         
     }
 
@@ -23,7 +41,11 @@ public class EnemySpawner : MonoBehaviour
     {
         if (Time.time >= nextSpawn)
         {
-            Instantiate(enemyPrefab, spawnLocation.transform.position, spawnLocation.transform.rotation);
+            float randomValueY = Random.Range(0, spawnAreaHeight);
+            float randomValueX = Random.Range(0, spawnAreaWidth);
+            randomSpawnPoint = new Vector3(randomValueX, randomValueY, 0);
+            // When timer resets, instantiate an enemy prefab
+            Instantiate(enemyPrefab, randomSpawnPoint, spawnLocation.transform.rotation);
             nextSpawn = Time.time + 1f / spawnRate;
         }
     }
